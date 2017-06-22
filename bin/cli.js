fbd41f11e7208f1cd0bd1e23fe7af8176a7d675a
cortney <contact@cortneyknorr.com>
2017-06-22 23:24:04 +0200

    Fixed ubuntu issues

--- bin/cli.js
+++ bin/cli.js
@@ -2,9 +2,9 @@
 
 const fs = require("fs");
 const path = require("path");
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
+const home = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -17,7 +17,7 @@ function ask(question){
 		rl.close();
 		return data;
 	});
-};
+}
 
 
 function execute(command) {
@@ -37,32 +37,12 @@ function execute(command) {
 switch(process.argv[2]){
 	case "init":
 		console.log("Downloading files");
-		execute("git clone https://github.com/CortneyKnorr/radix.git .radix")
+		execute("git clone https://github.com/CortneyKnorr/radix.git ./")
 			.then(data => {
-				console.log("Copying files");
-				return execute("cp .radix/{.gitignore,*.*} ./");
+				return execute("rm -fr ./.git");
 			})
 			.then(data => {
-				return execute("rm .radix/{.gitignore,*.*}");
-			})
-			.then(data => execute("cp -r .radix/* ./"))
-			.then(data => {
-				console.log("Radix files in place");
-				return ask("Do you want to install dependencies?(y,n)");
-			})
-			.then(data => {
-				if(data == 'y'){
-					console.log("This may take a while");
-					return execute("npm install");
-				};
-				return true;
-			})
-			.then(data => {
-				console.log("Cleanup");
-				return execute("rm -fr .radix");
-			})
-			.then(data => {
-                console.log("All done!")
+                console.log("All done!");
                 process.exit();
             })
 			.catch(error => {
@@ -71,30 +51,6 @@ switch(process.argv[2]){
 			})
 		;
 		break;
-    case "dependencies":
-        console.log("Installing global dependencies");
-        execute("sudo npm install -g nodemon")
-            .then(data => {
-                console.log("30% finished");
-                return execute("sudo npm install -g browser-sync");
-            })
-            .then(data => {
-                console.log("60% finished");
-                return execute("sudo npm install -g n");
-            })
-            .then(data => {
-                console.log("90% finished");
-                return execute("sudo n stable");
-            })
-            .then(data => {
-                console.log("Finished");
-                process.exit();
-            })
-            .catch(data => {
-                console.log(data);
-                console.log("Something went wrong");
-            })
-        break;
 	default:
 		let args = ["radix.js", ...process.argv.splice(2)];
 		let ls = require('child_process').spawn("node", args);
